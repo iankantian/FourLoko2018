57628a79457a4540fd6585c355dfdf04b159c4be
Joshua Brown <jbrown@ellation.com>
2017-11-12 22:54:27 -0800

    - added some right motor functions, working but I want to make it one function for either right or left motors

--- FourLoko.h
+++ FourLoko.h
@@ -1,31 +1,31 @@
 /**
- * FourLoko.h
- * Joshua Brown 2017
- * 
- * helper functions for FourLoko, my minisumo bot
- */
+   FourLoko.h
+   Joshua Brown 2017
+
+   helper functions for FourLoko, my minisumo bot
+*/
 
 void initPins() {
   pinMode(rightPx, INPUT);
-  pinMode(in1R, OUTPUT);
-  pinMode(pwmR, INPUT); // TODO: SWITCH TO OUTPUT FOR USING MOTORS
-  pinMode(pwmL, INPUT); // TODO: SWITCH TO OUTPUT FOR USING MOTORS
-  pinMode(in2L, OUTPUT);
+  pinMode(pwmR, OUTPUT); // TODO: SWITCH TO OUTPUT FOR USING MOTORS
+  pinMode(pwmL, OUTPUT); // TODO: SWITCH TO OUTPUT FOR USING MOTORS
   pinMode(in1L, OUTPUT);
+  pinMode(in2L, OUTPUT);
   pinMode(stdbyL, OUTPUT);
+  pinMode(stdbyR, OUTPUT);
+  pinMode(in1R, OUTPUT);
+  pinMode(in2R, OUTPUT);
   pinMode(irPwm, OUTPUT);
   pinMode(ctrPx, INPUT);
   pinMode(usrBtn1, INPUT);
   pinMode(usrBtn2, INPUT);
   pinMode(fLefttPx, INPUT);
   pinMode(leftPx, INPUT);
-  pinMode(in2R, OUTPUT);
   pinMode(vSense, INPUT);
   pinMode(edgeLeft, INPUT);
   pinMode(edgeRight, INPUT);
   pinMode(fRightPx, INPUT);
   pinMode(gyroZ, INPUT);
-  pinMode(stdbyR, OUTPUT);
 }
 
 void initIrPwm() {
@@ -53,7 +53,52 @@ void printOpponentSensors() {
 void updateGyroDisplacement() {
   int delta = analogRead(gyroZ) - zeroRateGyroZ;
   if (abs(delta) > zThetaDeadband) {
-      zThetaDisplacement = zThetaDisplacement + delta;
+    zThetaDisplacement = zThetaDisplacement + delta;
+  }
+}
+
+void rightCoast() {
+  digitalWrite(stdbyR, LOW);
+  digitalWrite(in2R, LOW);
+  digitalWrite(in1R, LOW);
+}
+
+void rightBrake() {
+  digitalWrite(stdbyR, HIGH);
+  digitalWrite(in2R, HIGH); // raising both IN's high or setting PWM LOW turns on brakes
+  digitalWrite(in1R, HIGH); // raising both IN's high or setting PWM LOW turns on brakes
+}
+
+void rightRotate(int velocity) {
+  if (velocity > 0) {
+    digitalWrite(in1R, HIGH);
+    digitalWrite(in2R, LOW);
+  } else {
+    digitalWrite(in1R, LOW);
+    digitalWrite(in2R, HIGH);
+    velocity = -velocity;
+  }
+
+  digitalWrite(pwmR, HIGH);
+  analogWrite(stdbyR, velocity);
+}
+
+void rightRotate(int velocity, bool braked) {
+  if (velocity > 0) {
+    digitalWrite(in1R, HIGH);
+    digitalWrite(in2R, LOW);
+  } else {
+    digitalWrite(in1R, LOW);
+    digitalWrite(in2R, HIGH);
+    velocity = -velocity;
+  }
+
+  if (braked) {
+    digitalWrite(stdbyR, HIGH);
+    analogWrite(pwmR, velocity);
+  } else {
+    digitalWrite(pwmR, HIGH);
+    analogWrite(stdbyR, velocity);
   }
 }
 
--- FourLoko2018.ino
+++ FourLoko2018.ino
@@ -10,4 +10,16 @@ void setup() {
 }
 
 void loop() {
+//  rightRotate(128);
+//  delay(50);
+//  rightCoast();
+//  delay(3000);
+//  rightRotate(128, true);
+//    delay(50);
+//  rightBrake();
+//  delay(3000);
+//  rightRotate(128, false);
+//  delay(50);
+//  rightCoast();
+//  delay(3000);
 }
